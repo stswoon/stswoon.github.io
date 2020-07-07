2f02602221bd4e816dc1861f93d34b9c3eb4a857
nekhozhin <nekhozhin@netcracker.com>
2020-07-07 23:22:43 +0400

    q

--- how-old-are-you/main.js
+++ how-old-are-you/main.js
@@ -1,43 +1,61 @@
-if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js', { scope: './' })
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', {scope: './'})
 }
 
 function saveDate() {
-	document.getElementById("change").style.display = "block";
-	document.getElementById("answer").style.display = "block";
-	document.getElementById("input").style.display = "none";
-	localStorage.setItem("date", document.getElementById("date").value);
-	fillAnswer();
+    document.getElementById("change").style.display = "block";
+    document.getElementById("answer").style.display = "block";
+    document.getElementById("input").style.display = "none";
+    var v = document.getElementById("day").value + "-" +
+        document.getElementById("month").value + "-" +
+        document.getElementById("year").value;
+    localStorage.setItem("date", v);
+    fillAnswer();
 }
 
 function showDateInput() {
-	document.getElementById("change").style.display = "none";
-	document.getElementById("answer").style.display = "none";
-	document.getElementById("input").style.display = "block";
+    document.getElementById("change").style.display = "none";
+    document.getElementById("answer").style.display = "none";
+    document.getElementById("input").style.display = "block";
 }
 
 function fillAnswer() {
-	var date = localStorage.getItem("date");
-	if (!date) {
-		showDateInput();
-		return;
+    var date = localStorage.getItem("date");
+    if (!date) {
+        showDateInput();
+        return;
+    }
+
+    document.getElementById("change").style.display = "block";
+    document.getElementById("answer").style.display = "block";
+    document.getElementById("input").style.display = "none";
+
+    date = date.split("-");
+    document.getElementById("day").value = date[0];
+    document.getElementById("month").value = date[1];
+    document.getElementById("year").value = date[2];
+
+    var now = new Date();
+    y = now.getFullYear() - date[2];
+    m = (now.getMonth() + 1) - date[1];
+    if (m < 0) {
+        m += 12;
+        y -= 1;
+    }
+    d = now.getDate() - date[0];
+    if (d < 0) {
+        m -= 1;
+    }
+	if (m < 0) {
+		m += 12;
+		y -= 1;
 	}
 
-	document.getElementById("change").style.display = "block";
-	document.getElementById("answer").style.display = "block";
-	document.getElementById("input").style.display = "none";
-
-	document.getElementById("date").value = date;
-
-	date  = new Date(date);
-	var now = new Date(Date.now());
-	y = now.getFullYear() - date.getFullYear();
-	m = now.getMonth() - date.getMonth();
-	d = now.getDate() - date.getDate();
-
-	document.getElementById("answerYear").innerText =
-		"Вам " + y;
-	document.getElementById("answerEtc").innerText =
-		m + " месяц(а/ев) и " + d + " д(ня/ней/ень)";
+    document.getElementById("answerYear").innerText =
+        "Вам " + y;
+    document.getElementById("answerEtc").innerText =
+    	"и " + m + " месяц(а/ев)";
+    // 	m + " месяц(а/ев) и " + d + " д(ня/ней/ень)";
 }
+
 fillAnswer();
--- how-old-are-you/sw.js
+++ how-old-are-you/sw.js
@@ -1,28 +1,29 @@
-const cacheName = 'HowOldAreYouCache-v4';
+const cacheName = 'HowOldAreYouCache-v5';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './main.js',
-		'./favicon.ico',
-		'./style.css'
-      ]);
-    })
-  );
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './main.js',
+                './favicon.ico',
+                './style.css',
+                './moment.min.js'
+            ]);
+        })
+    );
 });
 
 // Our service worker will intercept all fetch requests
 // and check if we have cached the file if so it will serve the cached file
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
