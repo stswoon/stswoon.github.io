c5bb11ed45e0b91bd6cd0981f7749d52afb2cba4
nekhozhin <nekhozhin@netcracker.com>
2020-07-07 22:16:28 +0400

    q

--- how-old-are-you/README.md
+++ how-old-are-you/README.md
@@ -1,2 +1,3 @@
-https://www.digitalocean.com/community/tutorials/js-vanilla-pwa
-https://www.ma-no.org/en/programming/javascript/how-to-build-a-basic-pwa-with-html-and-javascript
+1. https://www.digitalocean.com/community/tutorials/js-vanilla-pwa
+1. https://www.ma-no.org/en/programming/javascript/how-to-build-a-basic-pwa-with-html-and-javascript
+1. https://viblo.asia/p/creating-a-basic-progressive-web-app-using-vanillajs-m68Z0RVX5kG
--- how-old-are-you/main.js
+++ how-old-are-you/main.js
@@ -1,3 +1,42 @@
-    if('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('sw.js', { scope: './' })
-    }
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js', { scope: './' })
+}
+
+function saveDate() {
+	document.getElementById("change").style.display = "block";
+	document.getElementById("answer").style.display = "block";
+	document.getElementById("input").style.display = "none";
+	localStorage.setItem("date", document.getElementById("date").value);
+	fillAnswer();
+}
+
+function showDateInput() {
+	document.getElementById("change").style.display = "none";
+	document.getElementById("answer").style.display = "none";
+	document.getElementById("input").style.display = "block";
+}
+
+function fillAnswer() {
+	var date = localStorage.getItem("date");
+	if (!date) {
+		showDateInput();
+		return;
+	}
+
+	document.getElementById("change").style.display = "block";
+	document.getElementById("answer").style.display = "block";
+	document.getElementById("input").style.display = "none";
+
+	document.getElementById("date").value = date;
+
+	var period = new Date(Date.now() - new Date(date));
+	y = period.getFullYear() - 1970;
+	m = period.getMonth();
+	d = period.getDay();
+
+	document.getElementById("answerYear").innerText =
+		"Вам " + y;
+	document.getElementById("answerEtc").innerText =
+		m + " месяц(а/ев) и " + d + " д(ня/ней/ень)";
+}
+fillAnswer();
