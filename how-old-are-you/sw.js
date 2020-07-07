1ad7d5a2f60e9c7f5b6006d152911ec4855c77a6
nekhozhin <nekhozhin@netcracker.com>
2020-07-07 20:50:07 +0400

    test-pwa

---
+++ how-old-are-you/sw.js
@@ -0,0 +1,17 @@
+const cacheName = 'Temporas';
+
+// Cache all the files to make a PWA
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // Our application only has two files here index.html and manifest.json
+      // but you can add more such as style.css as your app grows
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+        './main.js'
+      ]);
+    })
+  );
+});
