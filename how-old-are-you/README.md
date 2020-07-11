9f16c64ddd274ca4693997f9436e1e0b23073aeb
nekhozhin <nekhozhin@netcracker.com>
2020-07-12 03:39:39 +0400

    PWA: how-old-are-yo

--- how-old-are-you/README.md
+++ how-old-are-you/README.md
@@ -2,3 +2,6 @@
 1. https://www.ma-no.org/en/programming/javascript/how-to-build-a-basic-pwa-with-html-and-javascript
 1. https://viblo.asia/p/creating-a-basic-progressive-web-app-using-vanillajs-m68Z0RVX5kG
 1. https://habr.com/ru/company/2gis/blog/345552/
+1. https://developer.mozilla.org/ru/docs/Web/API/Service_Worker_API/Using_Service_Workers
+1. https://medium.com/nuances-of-programming/service-workers-%D0%B8-%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D0%B8-%D0%BA%D1%8D%D1%88%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-a32dcaca1aa2
+1. https://habr.com/ru/post/358060/
--- how-old-are-you/sw.js
+++ how-old-are-you/sw.js
@@ -1,23 +1,40 @@
-const cacheName = 'HowOldAreYouCache-v0.4';
+const cacheName = 'HowOldAreYouCache-v0.5';
 
-self.addEventListener('install', e => {
-    e.waitUntil(
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './main.js',
+    './favicon.ico',
+    './style.css'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
         caches.open(cacheName).then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './main.js',
-                './favicon.ico',
-                './style.css',
-                './moment.min.js'
-            ]);
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => {
+                        return urlsToCache.indexOf(cacheName) !== -1
+                    })
+                    .map(cacheName => {
+                        return caches.delete(cacheName);
+                    })
+            );
         })
     );
 });
 
 // при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
     event.respondWith(fromCache(event.request));
     // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
